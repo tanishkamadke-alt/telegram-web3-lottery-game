dc99b3fb782d44503edef7b584005594f4b3be12
tanishkamadke-alt <tanishkamadke@gmail.com>
2026-07-14 02:17:56 +0530

    Milestone 2: Implement Lottery smart contract

---
+++ smart-contract/contracts/Lottery.sol
@@ -0,0 +1,129 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract Lottery{
+
+    //address of contract owner
+    address public manager;
+
+    //fixed ticket price
+    uint public ticketPrice = 0.01 ether;
+
+    //list of players
+    address[] public players;
+    uint public currentRound = 1;
+
+    address public winner;
+
+    address public latestWinner;
+
+    struct LotteryRound {
+        uint roundId;
+        address winner;
+        uint prizeAmount;
+        uint getTotalPlayers;
+        uint ticketPrice;
+        uint timestamp;
+        bytes32 lotteryHash;
+    }
+
+    LotteryRound[] public lotteryHistory;
+
+    event TicketPurchased(
+        address indexed player,
+        uint indexed roundId
+    );
+
+    event WinnerSelected(
+        uint indexed roundId,
+        address indexed winner,
+        uint prizeAmount
+    );
+
+    //modifier lets us write the check once and reuse Identifier
+    modifier onlyManager() {
+        require(msg.sender == manager, "Only manager can perform this action");
+        _;   //after the security check passes, execute the function
+    }
+
+    //constructor: save the wallet address of the person who deployed the contract
+    //that person becomes the manager (admin) of the Lottery
+    
+    constructor(){
+        manager = msg.sender;  //wallet address that called current function
+    } 
+
+    function buyTicket() public payable {
+        require(msg.value == ticketPrice, "Incorrect ticket price");
+        players.push(msg.sender);
+        emit TicketPurchased(msg.sender, currentRound);
+    }
+
+    function getPlayers() public view returns (address[] memory) {
+        return players;
+    }
+
+    function getTotalPlayers() public view returns (uint) {
+        return players.length;
+    }
+
+    function getContractBalance() public view returns (uint){
+        return address(this).balance;
+    }
+
+    function random() private view returns (uint){
+        //returns large unsigned integer
+        return uint(  
+            keccak256( //solidity's cryptographic hash function
+                abi.encodePacked(  //combines several values into one byte array before hashing
+                    block.timestamp,
+                    block.prevrandao,
+                    players.length
+                )
+            )
+        );
+    }
+
+    function drawWinner() public onlyManager{
+        require(players.length >= 2, "At least two players are required");
+
+        uint winnerIndex = random() % players.length;
+        address winnerAddress = players[winnerIndex];
+
+        latestWinner = winnerAddress;
+
+        uint prize = address(this).balance;
+
+        payable(winnerAddress).transfer(prize);
+
+        lotteryHistory.push(
+            LotteryRound({
+                roundId: currentRound,
+                winner: winnerAddress,
+                prizeAmount: prize,
+                getTotalPlayers: players.length,
+                ticketPrice: ticketPrice,
+                timestamp: block.timestamp,
+                lotteryHash: keccak256(
+                    abi.encodePacked(
+                        currentRound,
+                        winnerAddress,
+                        prize,
+                        block.timestamp
+                    )
+                )
+            })
+        );
+
+        emit WinnerSelected(
+            currentRound,
+            winnerAddress,
+            prize
+        );
+        delete players;
+
+        currentRound++;
+    }
+
+}
+
